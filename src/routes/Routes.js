e8db601ab8daf61e3b67a71b13c295c2889c9a37
risyad <risyadzyn@gmail.com>
2021-10-12 14:16:05 +0700

    sidebar and header

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import HeaderTime from "../components/header/HeaderTime"
 import Sidebar from "../components/sidebar/Sidebar"
 
 
@@ -6,7 +7,8 @@ import Sidebar from "../components/sidebar/Sidebar"
 const Routes = () => {
   return (
     <Router>
-      <Sidebar />
+      {/* <Sidebar /> */}\
+      <HeaderTime />
     </Router>
   )
 }
