033b491f4346a654ca836d7cbb056e91b87422f4
risyad <risyadzyn@gmail.com>
2021-11-01 20:42:49 +0700

    mau pull

--- src/components/print/PrintPage.js
+++ src/components/print/PrintPage.js
@@ -2,20 +2,23 @@ import { useState, useEffect, forwardRef } from "react";
 import "./Print.scss";
 import SafeIcon from "../../assets/icons/brangkas.svg";
 import MinusLine from '../../assets/indicators/short-line.svg'
-import { getReportMonthly, getSafe } from '../../services';
+import { getReportMonthly, getSafe, getTransaction } from '../../services';
 import NumberFormat from "react-number-format";
 import * as AiIcons from 'react-icons/ai'
 
 const PrintPage = forwardRef((props, ref) => {
+  const [transactions, setTransactions] = useState([])
   const [incomes, setIncomes] = useState([])
   const [expenseNet, setExpenseNet] = useState([])
   const [incomeNet, setIncomeNet] = useState([])
   const [totalIncome, setTotalIncome] = useState(0)
   const [totalExpense, setTotalExpense] = useState(0)
   const [safes, setSafes] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
   const token = localStorage.getItem('token')
 
   useEffect(() => {
+    setIsLoading(true)
     getReportMonthly()
       .then((res) => {
         setIncomes(res?.data?.addIncome)
@@ -26,9 +29,11 @@ const PrintPage = forwardRef((props, ref) => {
   }, [])
 
   useEffect(() => {
+    setIsLoading(true)
     getReportMonthly()
       .then((res) => {
         setIncomeNet(res?.data?.addIncome)
+        setIsLoading(false)
         const total = res?.data?.addIncome?.reduce((prev, curr) => {
           return prev + parseInt(curr.totalAddIncome)
         }, 0)
@@ -40,9 +45,11 @@ const PrintPage = forwardRef((props, ref) => {
   }, [])
 
   useEffect(() => {
+    setIsLoading(true)
     getReportMonthly()
       .then((res) => {
         setExpenseNet(res?.data?.expense)
+        setIsLoading(false)
         const total = res?.data?.expense?.reduce((prev, curr) => {
           return prev + parseInt(curr.totalExpense)
         }, 0)
@@ -54,13 +61,27 @@ const PrintPage = forwardRef((props, ref) => {
   }, [])
 
   useEffect(() => {
+    setIsLoading(true)
     getSafe(token)
       .then((res) => {
         setSafes(res?.data)
-
+        setIsLoading(false)
       })
       .catch((error) => {
         console.log(error)
+      })
+  }, [])
+
+  useEffect(() => {
+    setIsLoading(true)
+    getTransaction()
+      .then((res) => {
+        setTransactions(res?.data?.data?.transactions)
+        setIsLoading(false)
+      })
+      .catch((error) => {
+        console.log(error)
+        setIsLoading(false)
       })
   }, [])
 
@@ -86,7 +107,6 @@ const PrintPage = forwardRef((props, ref) => {
             <div className='income-wrapper'>
               <div className='section-title'>
                 <div>Income</div>
-                <hr />
                 {incomes.map((income) => {
                   return (
                     <NumberFormat
@@ -127,39 +147,39 @@ const PrintPage = forwardRef((props, ref) => {
             <div className='expense-wrapper'>
               <div className='section-title'>
                 <div>Expense</div>
-                <hr />
-                {incomes.map((income) => {
-                  return (
-                    <NumberFormat
-                      value={income.totalAddIncome}
-                      displayType="text"
-                      thousandSeparator="."
-                      decimalSeparator=","
-                      prefix="Rp"
-                    />
-                  )
-                })}
+                <NumberFormat
+                  value={expenseNet.reduce((prev, curr) => {
+                    return prev + parseInt(curr.totalExpense)
+                  }, 0)}
+                  displayType="text"
+                  thousandSeparator="."
+                  decimalSeparator=","
+                  prefix="Rp"
+                />
               </div>
               <div><hr /></div>
-              <div className='income-content'>
-                <div className='income-title-value'>
-                  <img src={SafeIcon} alt='Safe Name' />
-                  {safes?.map((safe) => {
-                    return (
-                      <div>To {safe.safeName}</div>
-                    )
-                  })}
-                </div>
-                {incomes.map((income) => {
-                  return (
-                    <NumberFormat
-                      value={income.totalAddIncome}
-                      displayType="text"
-                      thousandSeparator="."
-                      decimalSeparator=","
-                      prefix="Rp"
-                    />
-                  )
+              <div className='expense-content'>
+                {transactions.map((transaction) => {
+                  return transaction.type === 'expense' ? (
+                    <div className='expense-content-wrapper'>
+                      <img src={transaction.Categories.image_url} alt='Safe Name' />
+                      <div className='expense-content-value'>
+                        <div className='expense-title-value'>
+                          <div style={{ fontWeight: 'bold' }}>{transaction.Categories.categoryName}</div>
+                          <div>{transaction.detailExpense}</div>
+                        </div>
+                        <div>
+                          <NumberFormat
+                            value={transaction.expense}
+                            displayType="text"
+                            thousandSeparator="."
+                            decimalSeparator=","
+                            prefix="Rp"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  ) : ''
                 })}
               </div>
             </div>
@@ -212,14 +232,17 @@ const PrintPage = forwardRef((props, ref) => {
                 </div>
                 <div className='line-indicator'></div>
               </div>
-              <NumberFormat
-                className='minus-result'
-                value={netIncome}
-                displayType="text"
-                thousandSeparator="."
-                decimalSeparator=","
-                prefix="Rp"
-              />
+              <div className='minus-result'>
+                <div>Net Income</div>
+                <NumberFormat
+                  value={netIncome}
+                  displayType="text"
+                  thousandSeparator="."
+                  decimalSeparator=","
+                  prefix="Rp"
+                />
+              </div>
+
             </div>
 
             {/* Ending Balance */}
@@ -262,7 +285,6 @@ const PrintPage = forwardRef((props, ref) => {
                 })}
               </div>
             </div>
-
           </div>
         </div>
       </div>
